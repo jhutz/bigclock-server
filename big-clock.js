dbabc22d9e417018ed6145c00044f8bdc8127119
Jeffrey Hutzelman <jhutz@cmu.edu>
2017-06-04 03:26:25 -0400

    Version 1.1

--- big-clock.js
+++ big-clock.js
@@ -21,7 +21,7 @@ var f_v_srv    = document.getElementById("version_srv");
 var f_v_opts   = document.getElementById("version_opts");
 
 var css_version = "";
-var js_version = "1.0-@@@@@@-@@@@@@";
+var js_version = "1.1-@@@@@@-@@@@@@";
 var data_port = "9999";
 
 var timezone = "";
